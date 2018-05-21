dcc49f227034428d0e6c273d05e40c90d70eda55
keitheck <eckertkeith@gmail.com>
2018-05-21 13:44:26 -0700

    user model defined

--- lwb_heartbeat/lwb_heartbeat/urls.py
+++ lwb_heartbeat/lwb_heartbeat/urls.py
@@ -16,16 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from .views import home_view
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('accounts/', include('registration.backends.hmac.urls')),
+    path('users/', include('users.urls')),
 ]
 
 
 # use if our static images stop loading to the view:
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
