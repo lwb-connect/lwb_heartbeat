74e41fee18d1ff314d58c565a10d620f532c5409
keitheck <eckertkeith@gmail.com>
2018-05-23 08:09:38 -0700

    model development

--- lwb_heartbeat/child/admin.py
+++ lwb_heartbeat/child/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Child
+from .models import Child, LWBProgram, MedicalCodes, Country, Education
 
 # Register your models here.
+admin.site.register((Country))
 admin.site.register((Child))
+admin.site.register((LWBProgram))
+admin.site.register((MedicalCodes))
+admin.site.register((Education))
